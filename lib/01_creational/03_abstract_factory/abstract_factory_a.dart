d24180ea403287bd431940ae49f09859c9735bb7
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-20 18:32:19 -0300

    Implement abstract factory pattern for Hamburger and Drink classes with specific implementations for FastFood and Healthy restaurants

--- lib/01_creational/03_abstract_factory/abstract_factory_a.dart
+++ lib/01_creational/03_abstract_factory/abstract_factory_a.dart
@@ -1,3 +1,72 @@
+import '../../utils/print_with_color.dart';
+
+abstract class Hamburger {
+  void prepare();
+}
+
+abstract class Drink {
+  void pour();
+}
+
+class ChickenHamburger implements Hamburger {
+  @override
+  void prepare() {
+    printWithColor('Preparando hambúrguer de ', 'frango', 'yellow');
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  @override
+  void prepare() {
+    printWithColor('Preparando hambúrguer de carne ', 'bovina', 'brown');
+  }
+}
+
+class Water implements Drink {
+  @override
+  void pour() {
+    printWithColor('Servindo ', 'água', 'blue');
+  }
+}
+
+class Soda implements Drink {
+  @override
+  void pour() {
+    printWithColor('Servindo ', 'refrigerante', 'orange');
+  }
+}
+
+abstract class RestaurantFactory {
+  Hamburger createHamburger();
+  Drink createDrink();
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  @override
+  Hamburger createHamburger() => BeefHamburger();
+
+  @override
+  Drink createDrink() => Soda();
+}
+
+class HealthyRestaurantFactory implements RestaurantFactory {
+  @override
+  Hamburger createHamburger() => ChickenHamburger();
+
+  @override
+  Drink createDrink() => Water();
+}
+
+void runRestaurant(RestaurantFactory factory, String name, String color) {
+  printWithColor('\n', name, color);
+  print('-' * 40);
+  final hamburger = factory.createHamburger();
+  final drink = factory.createDrink();
+  hamburger.prepare();
+  drink.pour();
+}
+
 void main() {
-  // TODO: Implement ${file}
+  runRestaurant(FastFoodRestaurantFactory(), 'Fast Food Restaurant', 'red');
+  runRestaurant(HealthyRestaurantFactory(), 'Healthy Restaurant', 'green');
 }
--- lib/01_creational/03_abstract_factory/abstract_factory_b.dart
+++ lib/01_creational/03_abstract_factory/abstract_factory_b.dart
@@ -1,3 +1,74 @@
+import '../../utils/print_with_color.dart';
+
+abstract class Vehicle {
+  void assemble();
+}
+
+abstract class Engine {
+  void start();
+}
+
+class ElectricCar implements Vehicle {
+  @override
+  void assemble() {
+    print('Fabricando um carro elétrico.');
+  }
+}
+
+class GasCar implements Vehicle {
+  @override
+  void assemble() {
+    print('Fabricando um carro a combustão.');
+  }
+}
+
+class ElectricEngine implements Engine {
+  @override
+  void start() {
+    print('Ligando o motor elétrico.');
+  }
+}
+
+class GasEngine implements Engine {
+  @override
+  void start() {
+    print('Ligando o motor a combustão.');
+  }
+}
+
+abstract class VehicleFactory {
+  Vehicle createVehicle();
+  Engine createEngine();
+}
+
+class ElectricVehicleFactory implements VehicleFactory {
+  @override
+  Vehicle createVehicle() => ElectricCar();
+
+  @override
+  Engine createEngine() => ElectricEngine();
+}
+
+class GasVehicleFactory implements VehicleFactory {
+  @override
+  Vehicle createVehicle() => GasCar();
+
+  @override
+  Engine createEngine() => GasEngine();
+}
+
+void runFactory(
+    VehicleFactory factory, String leading, String colored, String color) {
+  printWithColor(leading, colored, color);
+  print('-' * 40);
+  final vehicle = factory.createVehicle();
+  final engine = factory.createEngine();
+  vehicle.assemble();
+  engine.start();
+}
+
 void main() {
-  // TODO: Implement ${file}
+  runFactory(
+      ElectricVehicleFactory(), 'Criando veículo ', 'elétrico:', 'yellow');
+  runFactory(GasVehicleFactory(), '\nCriando veículo a ', 'combustão:', 'red');
 }
