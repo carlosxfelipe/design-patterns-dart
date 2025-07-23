036b1707c4439d0bbadd2b5d0f0805bb6b17e01b
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-22 22:21:53 -0300

    Implement prototype pattern for Document and Pokemon classes with cloning and display functionality

--- lib/01_creational/04_prototype/prototype_a.dart
+++ lib/01_creational/04_prototype/prototype_a.dart
@@ -1,3 +1,36 @@
+import 'package:design_patterns_dart/utils/print_with_color.dart';
+
+class Document {
+  String title;
+  final String _content;
+  String author;
+
+  Document(this.title, String content, this.author) : _content = content;
+
+  void displayInfo() {
+    print('Title: $title');
+    print('Author: $author');
+    print('Content: $_content');
+  }
+
+  Document clone() {
+    return Document(title, _content, author);
+  }
+}
+
 void main() {
-  // TODO: Implement ${file}
+  final doc1 = Document(
+    'O guia de Dart',
+    'Fundamentos, prática, conceitos avançados e tudo mais',
+    'Julio Bitencourt',
+  );
+
+  printWithColor('', 'doc1:', 'blue');
+  doc1.displayInfo();
+
+  final doc2 = doc1.clone();
+  doc2.title = 'O guia avançado de Dart';
+
+  printWithColor('\n', 'doc2:', 'green');
+  doc2.displayInfo();
 }
--- lib/01_creational/04_prototype/prototype_b.dart
+++ lib/01_creational/04_prototype/prototype_b.dart
@@ -1,3 +1,38 @@
+class Pokemon {
+  String name;
+  String type;
+  int level;
+  List<String> attacks;
+
+  Pokemon(this.name, this.type, this.level, List<String> attacks)
+      : attacks = List.from(attacks);
+
+  Pokemon clone() {
+    return Pokemon(name, type, level, List.from(attacks));
+  }
+
+  void displayInfo() {
+    print('Nome: $name');
+    print('Tipo: $type');
+    print('Nível: $level');
+    print('Ataques: ${attacks.join(", ")}');
+  }
+}
+
 void main() {
-  // TODO: Implement ${file}
+  final pikachu = Pokemon("Pikachu", "Elétrico", 5, [
+    "Choque do Trovão",
+    "Ataque Rápido",
+  ]);
+
+  final charmander = Pokemon("Charmander", "Fogo", 5, [
+    "Lança-chamas",
+    "Garra de Fogo",
+  ]);
+
+  final clonedPikachu = pikachu.clone();
+  final clonedCharmander = charmander.clone();
+
+  clonedPikachu.displayInfo();
+  clonedCharmander.displayInfo();
 }
